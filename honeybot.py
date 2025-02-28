4d1381f3f1c64e9eac300110ed7b652ad4ed3ee5
Albert Veli <albert.veli@gmail.com>
2025-02-28 13:27:11 +0100

    Put the bot in a class

--- honeybot.py
+++ honeybot.py
@@ -13,97 +13,98 @@ except ImportError:
     print("if you're on a Debian-based system 'apt install python3-discord python3-openai python3-dotenv'")
     exit(1)
 
-openai_client = None
-config = None
+class HoneyBot:
+    def __init__(self):
+        self.openai_client = None
+        self.config = None
+        self.load_secrets()
 
-def load_secrets():
-    global openai_client, config
-    # Load API key and organization from .env file
-    # Don't forget to add .env to your .gitignore file
-    path = os.path.dirname(os.path.realpath(__file__)) + os.sep
-    if not os.path.exists(path + '.env'):
-        print('Please create a .env file with your OpenAI API key, organization and Discord bot token')
-        sys.exit(1)
-    config = dotenv_values(path + ".env")
-    openai_client = AsyncOpenAI(api_key = config['OPENAI_API_KEY'], organization = config['OPENAI_ORGANIZATION'])
-    if openai_client is None:
-        print('Could not load valid OpenAI API key and organization from .env file')
-        sys.exit(1)
+        self.intents = discord.Intents.default()
+        self.intents.message_content = True
+        self.client = discord.Client(intents=self.intents)
+        self.client.event(self.on_ready)
+        self.client.event(self.on_message)
 
-async def howto(question, mode='ubuntu'):
-    m = 'gpt-4o'
-    system = 'You are a CLI assistant. Provide only the command, no explanations or extra text.'
-    prompt = f'Provide the {mode} command-line command to ' + question
-    try:
-        response = await openai_client.chat.completions.create(model = m, messages = [{'role': 'system', 'content': system}, {'role': 'user', 'content': prompt}])
-    except Exception as e:
-        return f'Error: {str(e)}'
-    md = response.model_dump()
-    text = md['choices'][0]['message']['content']
-    text = text.strip()
-    if text.startswith('``'):
-        lines = text.split('\n')
-        if len(lines) > 1:
-            text = lines[1]
-    if text.startswith('`') or text.startswith('"') or text.startswith("'"):
-        text = text[1:-1]
-    return text
+    def load_secrets(self):
+        # Load API key and organization from .env file
+        path = os.path.dirname(os.path.realpath(__file__)) + os.sep
+        if not os.path.exists(path + '.env'):
+            print('Please create a .env file with your OpenAI API key, organization and Discord bot token')
+            sys.exit(1)
+        self.config = dotenv_values(path + ".env")
+        self.openai_client = AsyncOpenAI(api_key=self.config['OPENAI_API_KEY'], organization=self.config['OPENAI_ORGANIZATION'])
+        if self.openai_client is None:
+            print('Could not load valid OpenAI API key and organization from .env file')
+            sys.exit(1)
 
-load_secrets()
+    async def howto(self, question, mode='ubuntu'):
+        m = 'gpt-4o'
+        system = 'You are a CLI assistant. Provide only the command, no explanations or extra text.'
+        prompt = f'Provide the {mode} command-line command to ' + question
+        try:
+            response = await self.openai_client.chat.completions.create(model=m, messages=[{'role': 'system', 'content': system}, {'role': 'user', 'content': prompt}])
+        except Exception as e:
+            return f'Error: {str(e)}'
+        md = response.model_dump()
+        text = md['choices'][0]['message']['content']
+        text = text.strip()
+        if text.startswith('``'):
+            lines = text.split('\n')
+            if len(lines) > 1:
+                text = lines[1]
+        if text.startswith('`') or text.startswith('"') or text.startswith("'"):
+            text = text[1:-1]
+        return text
 
-intents = discord.Intents.default()
-intents.message_content = True
-client = discord.Client(intents=intents)
+    def morn(self):
+        things = [
+            'chips', 'husdjur', 'demoner', 'jesus', 'popcorn', 'bullar', 'saft', 'ärtor', 'pengar', 'godis', 'kaffe', 'te', 'kebab', 'pizza', 'paket', 'mamma', 'tandkräm', 'gröt', 'AK47', 'proteinshake', 'ägg', 'pannkakor', 'fiskbullar', 'köttbullar', 'spaghetti', 'nudlar', 'flingor', 'apelsinjuice', 'ostmackor', 'havregryn', 'strumpor', 'byxor', 'solrosfrön', 'senap', 'ketchup', 'små rymdgubbar', 'tomtenissar', 'ostbågar', 'rostad majs', 'lillasyster', 'citronbitare', 'fiskpinnar', 'knäckebröd', 'julmust'
+        ]
+        places = [
+            'på spisen', 'i ugnen', 'i torkskåpet', 'under sängen', 'bakom soffan', 'på dagis', 'hos rektorn', 'i himlen', 'i hallen', 'i kyrkan', 'i fotöljen', 'i gondolen', 'i Sherwoodskogen', 'i kaffekoppen', 'i blodomloppet', 'i magen', 'i flaskan', 'i badrumsskåpet', 'i toaletten', 'på nätet', 'på toasitsen', 'på golvet', 'på vinden', 'på taket', 'på stolen', 'på hatthyllan', 'på varmvattenpumpen', 'på bänken', 'i båten', 'på steam', 'på ICA', 'på netflix', 'på smörgåsbordet', 'på terassen', 'i kylen', 'i Tyskland', 'i ryggsäcken', 'i postlådan', 'i magväskan', 'i kaffekoppen', 'i fickan', 'i tomtens hatt', 'i skogen', 'i micron', 'i handfatet', 'i tänderna', 'i helvetet'
+        ]
+        return f'Morn, {random.choice(things)} finns {random.choice(places)}'
 
-# TODO: move morn to a separate file
-things = [
-    'chips', 'husdjur', 'demoner', 'jesus', 'popcorn', 'bullar', 'saft', 'ärtor', 'pengar', 'godis', 'kaffe', 'te', 'kebab', 'pizza', 'paket', 'mamma', 'tandkräm', 'gröt', 'AK47', 'proteinshake', 'ägg', 'pannkakor', 'fiskbullar', 'köttbullar', 'spaghetti', 'nudlar', 'flingor', 'apelsinjuice', 'ostmackor', 'havregryn', 'strumpor', 'byxor', 'solrosfrön', 'senap', 'ketchup', 'små rymdgubbar', 'tomtenissar', 'ostbågar', 'rostad majs', 'lillasyster', 'citronbitare', 'fiskpinnar', 'knäckebröd', 'julmust'
-]
+    def commands(self):
+        return [
+            '!howto <question> - Get a Ubuntu command-line for a specific task',
+            '!powershell <question> - Get a PowerShell command-line for a specific task',
+            '!morn - Get a random "Morn" message',
+            '!vecka - Get the current week number',
+            '!commands - List available commands'
+        ]
 
-places = [
-    'på spisen', 'i ugnen', 'i torkskåpet', 'under sängen', 'bakom soffan', 'på dagis', 'hos rektorn', 'i himlen', 'i hallen', 'i kyrkan', 'i fotöljen', 'i gondolen', 'i Sherwoodskogen', 'i kaffekoppen', 'i blodomloppet', 'i magen', 'i flaskan', 'i badrumsskåpet', 'i toaletten', 'på nätet', 'på toasitsen', 'på golvet', 'på vinden', 'på taket', 'på stolen', 'på hatthyllan', 'på varmvattenpumpen', 'på bänken', 'i båten', 'på steam', 'på ICA', 'på netflix', 'på smörgåsbordet', 'på terassen', 'i kylen', 'i Tyskland', 'i ryggsäcken', 'i postlådan', 'i magväskan', 'i kaffekoppen', 'i fickan', 'i tomtens hatt', 'i skogen', 'i micron', 'i handfatet', 'i tänderna', 'i helvetet'
-]
+    async def vecka_nu(self):
+        cmd = ["curl", "-s", "https://vecka.nu/", "-H", "user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36"]
+        process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+        output, _ = subprocess.Popen(["html2markdown"], stdin=process.stdout, stdout=subprocess.PIPE).communicate()
+        return output.decode("utf-8").strip()
 
-def morn():
-    return f'Morn, {random.choice(things)} finns {random.choice(places)}'
+    async def on_ready(self):
+        print(f'Logged in as {self.client.user}')
 
-async def vecka_nu():
-    cmd = [ "curl", "-s", "https://vecka.nu/", "-H", "user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36" ]
-    # Pipe output from curl to html2markdown
-    process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-    output, error = subprocess.Popen(["html2markdown"], stdin=process.stdout, stdout=subprocess.PIPE).communicate()
-    return output.decode("utf-8").strip()
+    async def on_message(self, message):
+        if message.author == self.client.user:
+            return
+        lower = message.content.lower()
+        if message.channel.id == int(self.config['DISCORD_CHANNEL_ID']):
+            if lower.startswith('!howto'):
+                question = message.content[6:]
+                await message.channel.send(await self.howto(question))
+            elif lower.startswith('!powershell'):
+                question = message.content[11:]
+                await message.channel.send(await self.howto(question, mode='powershell'))
+            elif lower.startswith('!morn'):
+                await message.channel.send(self.morn())
+            elif lower.startswith('!vecka'):
+                await message.channel.send(await self.vecka_nu())
+            elif lower.startswith('!commands'):
+                for line in self.commands():
+                    await message.channel.send(line)
 
-@client.event
-async def on_ready():
-    print(f'Logged in as {client.user}')
+    def run(self):
+        self.client.run(self.config['DISCORD_BOT_TOKEN'])
 
-@client.event
-async def on_message(message):
-    #print(f"Mottaget: '{message.content}' från {message.author} i kanal {message.channel.id}")
-    if message.author == client.user:
-        print('response: ' + message.content)
-        # Ignore my own messages
-        return
-
-    # Respond to messages in the correct channel
-    if message.channel.id == int(config['DISCORD_CHANNEL_ID']):
-        if message.content.lower().startswith('!howto'):
-            question = message.content[6:]
-            print('!howto' + question)
-            await message.channel.send(await howto(question))
-
-        elif message.content.lower().startswith('!powershell'):
-            question = message.content[11:]
-            print('!powershell' + question)
-            await message.channel.send(await howto(question, mode='powershell'))
-
-        elif message.content.lower().startswith('!morn'):
-            # morn returns immediately, no need to await
-            await message.channel.send(morn())
-
-        elif message.content.lower().startswith('!vecka'):
-            await message.channel.send(await vecka_nu())
-
-client.run(config['DISCORD_BOT_TOKEN'])
-
+# main
+bot = HoneyBot()
+bot.run()
